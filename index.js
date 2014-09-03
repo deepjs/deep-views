3ef82b638652118a0bfec40529e0b75ca91dd363
Gilles Coomans <gilles.coomans@gmail.com>
2014-09-03 14:08:11 +0200

    follow deepjs refactoring (aup + directive parser)

--- index.js
+++ index.js
@@ -6,6 +6,6 @@
 if (typeof define !== 'function') {
     var define = require('amdefine')(module);
 }
-define(["require", "deepjs/deep", "./lib/view", "./lib/directives", "./lib/directives-parser", "./lib/dom-sheeter"], function(require, deep, views, directives, directivesParser, domsheets){
+define(["require", "deepjs/deep", "./lib/view", "./lib/directives", "./lib/dom-sheeter"], function(require, deep, views, directives, domsheets){
 	return deep;
 });
