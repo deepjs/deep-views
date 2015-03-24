30fc65627108bd1df7396aece6c290054baec5d6
Gilles Coomans <gilles.coomans@gmail.com>
2015-03-24 17:26:35 +0100

    clean

--- index.js
+++ index.js
@@ -6,6 +6,6 @@
 if (typeof define !== 'function') {
     var define = require('amdefine')(module);
 }
-define(["require", "deepjs/deep", "./lib/view", "./lib/directives", "./lib/dom-sheeter"], function(require, deep, views, directives, domsheets){
+define(["require", "deepjs/deep", "./lib/view", "./lib/directives", "./lib/dom-composer"], function(require, deep, views, directives, domsheets){
 	return deep;
 });
